1a92f77f6f3e65bd4ed8c82bb00a8dd46fd30001
ecrona <ecrona@gmail.com>
2016-09-16 10:34:55 +0200

    Moved styles out of the template file

--- public/components/com_cars/views/cars/view.html.php
+++ public/components/com_cars/views/cars/view.html.php
@@ -6,6 +6,9 @@ class CarsViewCars extends JViewLegacy
 {
 	public function display($tpl = null)
 	{
+		$document = JFactory::getDocument();
+		$document->addStylesheet(JUri::base() . 'components/com_cars/assets/style/style.css');
+
 		return parent::display($tpl);
 	}
 }
